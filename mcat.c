953a0fc0acaa24d87b5d849edecc610c8127298d
LeVanDuy3397 <97088590+LeVanDuy3397@users.noreply.github.com>
2024-05-28 12:49:52 +0700

    Update mcat.c

--- mcat.c
+++ mcat.c
@@ -40,7 +40,5 @@ int main (int argc, char *argv[])
       /* Unmap files (optional). */
       munmap (map);
       close(fd);
-    }
-  int aaa;
-  return EXIT_SUCCESS;
+    } return EXIT_SUCCESS;
 }
