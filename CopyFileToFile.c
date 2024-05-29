986ffd0a0bfc7d614894cee9a51a9cf4b81cd411
LeVanDuy3397 <levanduy1482k2@gmail.com>
2024-05-29 11:32:04 +0700

    read file to file version 3

--- CopyFileToFile.c
+++ CopyFileToFile.c
@@ -12,7 +12,7 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <ctype.h>
-
+#include <locale.h>
 
 int main (int argc, char *argv[]) 
 {
@@ -21,8 +21,7 @@ int main (int argc, char *argv[])
   const char * FileIn="/home/duy/MemoryMapedFiles/FileIn.txt";
   const char * FileOut="/home/duy/MemoryMapedFiles/FileOut.txt";
   void *in_data = (void *) 0x10000000;
-  void *out_data = (void *) 0x20000000;
-  int size;
+  void *out_data = (void *) 0x09000000;
 
   /* Open input file. */
   in_fd = open(FileIn, O_RDWR, S_IRUSR | S_IWUSR); 
@@ -50,26 +49,33 @@ int main (int argc, char *argv[])
         return EXIT_FAILURE;   
     }
   /* Map files. */
-  void *in_map = mmap(in_data, size, PROT_READ | PROT_WRITE, MAP_SHARED, in_fd, 0);
+  void *in_map = mmap(in_data, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, in_fd, 0);/*0 la vi tri bat dau de doc trong file ngoai,
+  sau khi doc duoc den vi tri bang file_size->dich chuyen den vi tri tiep theo trong file de doc tiep*/
   if (in_map == MAP_FAILED) 
     {
       printf ("%s: mmap in_map failed\n", FileIn);
       return EXIT_FAILURE;
     }
-  void *out_map = mmap(out_data, size, PROT_READ | PROT_WRITE, MAP_SHARED, out_fd, 0);
+
+  write(1, in_map, file_size);
+  printf("\n");
+  
+  void *out_map = mmap(out_data, file_size, PROT_READ | PROT_WRITE, MAP_SHARED, out_fd, 0);
   if (out_map == MAP_FAILED)
     {
       printf ("%s: mmap out_map failed\n", FileOut);
       return EXIT_FAILURE;
     }
+  write(1, out_map, file_size);
+  printf("\n");
 
   /* Copy files. */
-  memcpy (out_data, in_data, size);
+  memcpy (out_map, in_map, file_size);
 
   /* Unmap files (optional). */
-  munmap (in_map, size);
-  munmap (out_map, size);
-  
+  munmap (in_map, file_size);
+  munmap (out_map, file_size);
+
   return EXIT_SUCCESS;
 }
 
