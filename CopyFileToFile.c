e35b3a01466f51a3a2d4a2b82c004751393b009d
LeVanDuy3397 <levanduy1482k2@gmail.com>
2024-05-29 17:36:29 +0700

    copy file to file version 6: han che la file_size > page_size, chi sua code

--- CopyFileToFile.c
+++ CopyFileToFile.c
@@ -82,95 +82,95 @@ int main (int argc, char *argv[])
   return EXIT_SUCCESS;
 }
 
-      //  #include <fcntl.h>
-      //  #include <stdio.h>
-      //  #include <stdlib.h>
-      //  #include <sys/mman.h>
-      //  #include <sys/stat.h>
-      //  #include <unistd.h>
+       #include <fcntl.h>
+       #include <stdio.h>
+       #include <stdlib.h>
+       #include <sys/mman.h>
+       #include <sys/stat.h>
+       #include <unistd.h>
 
-      //  #define handle_error(msg) \
-      //      do { perror(msg); exit(EXIT_FAILURE); } while (0)
+       #define handle_error(msg) \
+           do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-      //  int main(int argc, char *argv[])
-      //  {
-      //      int          fd,i;
-      //      void         *map;
-      //      off_t        offset, pa_offset;
-      //      size_t       length;
-      //      ssize_t      s;
-      //      struct stat  sb;
-      //     char *fakeArgs[] = {"CopyFileToFile", "FileIn.txt", "FileOut.txt", "Demo.txt", NULL};
+       int main(int argc, char *argv[])
+       {
+           int          fd,i;
+           void         *map;
+           off_t        offset, pa_offset;
+           size_t       length;
+           ssize_t      s;
+           struct stat  sb;
+          char *fakeArgs[] = {"CopyFileToFile", "FileIn.txt", "FileOut.txt", "Demo.txt", NULL};
 
-      //     // Gán giá trị mới cho argc và argv
-      //     argc = sizeof(fakeArgs) / sizeof(fakeArgs[0])-1;
-      //     argv = fakeArgs;
+          // Gán giá trị mới cho argc và argv
+          argc = sizeof(fakeArgs) / sizeof(fakeArgs[0])-1;
+          argv = fakeArgs;
 
-      //     // In ra tất cả các đối số dòng lệnh
-      //     for (i = 0; i < argc; i++) {
-      //         fprintf("argv[%d]: %s\n", i, argv[i]);
-      //     }
+          // In ra tất cả các đối số dòng lệnh
+          for (i = 0; i < argc; i++) {
+              fprintf("argv[%d]: %s\n", i, argv[i]);
+          }
 
-      //     fprintf("%d \n", argc);
-      //      if (argc < 3 || argc > 4) {
-      //          fprintf("%s file offset \n", argv[0]);
-      //          exit(EXIT_FAILURE);
-      //      }
+          fprintf("%d \n", argc);
+           if (argc < 3 || argc > 4) {
+               fprintf("%s file offset \n", argv[0]);
+               exit(EXIT_FAILURE);
+           }
 
-      //      fd = open(argv[1], O_RDONLY);
-      //      if (fd == -1)
-      //          handle_error("open");
+           fd = open(argv[1], O_RDONLY);
+           if (fd == -1)
+               handle_error("open");
 
-      //      if (fstat(fd, &sb) == -1)           /* To obtain file size */
-      //          handle_error("fstat");
+           if (fstat(fd, &sb) == -1)           /* To obtain file size */
+               handle_error("fstat");
 
-      //      offset = 0x2400;
-      //      printf("kich thuoc trang %d \n",sysconf(_SC_PAGE_SIZE));// co dinh kich thuoc cua mot trang 4096 bytes ~ 4KB=4MB
-      //      pa_offset = offset & ~(sysconf(_SC_PAGE_SIZE) - 1);
-      //          /* offset for mmap() must be page aligned, tuc lam cho offset vi tri bat dau cua file luon phai la boi so cua trang*/
+           offset = 0x2400;
+           printf("kich thuoc trang %d \n",sysconf(_SC_PAGE_SIZE));// co dinh kich thuoc cua mot trang 4096 bytes ~ 4KB=4MB
+           pa_offset = offset & ~(sysconf(_SC_PAGE_SIZE) - 1);
+               /* offset for mmap() must be page aligned, tuc lam cho offset vi tri bat dau cua file luon phai la boi so cua trang*/
                
-      //      printf("offset truoc khi can chinh %d\n", offset);
-      //      printf("offset sau khi can chinh %d \n", pa_offset);
-      //      printf("kich thuoc cua file in %ld\n", sb.st_size); 
+           printf("offset truoc khi can chinh %d\n", offset);
+           printf("offset sau khi can chinh %d \n", pa_offset);
+           printf("kich thuoc cua file in %ld\n", sb.st_size); 
         
 
-      //      if (offset >= sb.st_size) {
-      //          fprintf(stderr, "offset is past end of file\n");
-      //          exit(EXIT_FAILURE);
-      //      }
+           if (offset >= sb.st_size) {
+               fprintf(stderr, "offset is past end of file\n");
+               exit(EXIT_FAILURE);
+           }
 
-      //      if (argc == 4) {
-      //          length = 100000;
-      //          //atoi(argv[1]);
-      //          if (offset + length > sb.st_size)
-      //              length = sb.st_size - offset;
-      //              printf("do dai length %d \n",length);
-      //                  /* Can't display bytes past end of file */
+           if (argc == 4) {
+               length = 100000;
+               //atoi(argv[1]);
+               if (offset + length > sb.st_size)
+                   length = sb.st_size - offset;
+                   printf("do dai length %d \n",length);
+                       /* Can't display bytes past end of file */
 
-      //      } else {    /* No length arg ==> display to end of file */
-      //          length = sb.st_size - offset;
-      //      }
-      //      //length = sb.st_size - offset;
-      //      map = mmap(NULL, length + offset - pa_offset, PROT_READ,
-      //                  MAP_PRIVATE, fd, pa_offset);
-      //      if (map == MAP_FAILED)
-      //          handle_error("mmap");
-      //      printf("-------------\n");
-      //      s = write(STDOUT_FILENO, map + offset - pa_offset, length);
-      //      if (s != length) {
-      //          if (s == -1)
-      //              handle_error("write");
+           } else {    /* No length arg ==> display to end of file */
+               length = sb.st_size - offset;
+           }
+           //length = sb.st_size - offset;
+           map = mmap(NULL, length + offset - pa_offset, PROT_READ,
+                       MAP_PRIVATE, fd, pa_offset);
+           if (map == MAP_FAILED)
+               handle_error("mmap");
+           printf("-------------\n");
+           s = write(STDOUT_FILENO, map + offset - pa_offset, length);
+           if (s != length) {
+               if (s == -1)
+                   handle_error("write");
 
-      //          fprintf(stderr, "partial write");
-      //          exit(EXIT_FAILURE);
-      //      }
-      //      printf("\n -------------\n");
-      //      printf("Da in ra thanh cong \n");
-      //      munmap(map, length + offset - pa_offset);
-      //      close(fd);
+               fprintf(stderr, "partial write");
+               exit(EXIT_FAILURE);
+           }
+           printf("\n -------------\n");
+           printf("Da in ra thanh cong \n");
+           munmap(map, length + offset - pa_offset);
+           close(fd);
 
-      //      exit(EXIT_SUCCESS);
-      //  }
+           exit(EXIT_SUCCESS);
+       }
 
 
 
